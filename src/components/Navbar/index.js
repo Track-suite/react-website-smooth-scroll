126b5749c0e827fba80423b4a5bb2e78097834f5
Asram Ahamed <asrama1998@gmail.com>
2022-01-18 16:51:12 +0530

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+          <NavBarContainer>
+              <NavLogo>Dollo</NavLogo>
+          </NavBarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
