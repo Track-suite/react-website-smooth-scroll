a1a62fd2a5827deacae996f32703451312038072
Asram Ahamed <asrama1998@gmail.com>
2022-01-20 12:19:16 +0530

    created nav bar and menu icon

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,31 @@
 import React from "react";
+import {FaBars} from 'react-icons/fa'
+import {
+  Nav,
+  NavBarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks
+} from "./NavBarElements";
+
 
 const Navbar = () => {
   return (
     <>
       <Nav>
-          <NavBarContainer>
-              <NavLogo>Dollo</NavLogo>
-          </NavBarContainer>
+        <NavBarContainer>
+          <NavLogo to="/">Dollo</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </NavBarContainer>
       </Nav>
     </>
   );
